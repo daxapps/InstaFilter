bd19d7d6c1804246ccdafe407699a987ac9aa64b
daxapps <daxapps777@gmail.com>
2017-03-26 13:41:06 -0500

    Save filtered photo to photo library

--- InstaFilter/ViewController.swift
+++ InstaFilter/ViewController.swift
@@ -66,6 +66,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func save(_ sender: Any) {
+        
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
 
     @IBAction func intensityChanged(_ sender: Any) {
@@ -111,5 +113,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = processedImage
         }
     }
+    
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo:UnsafeRawPointer) {
+        
+        if let error = error {
+            
+            //we got back an error!
+            
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            present(ac, animated: true)
+            
+        } else {
+            
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            present(ac, animated: true)
+        }
+    }
+    
 }
 
